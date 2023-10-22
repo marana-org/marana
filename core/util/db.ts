0f5d16e6f610b9964ce168dc6d86ba593d8fff68
Levi R <levir620@skiff.com>
2023-10-21 19:40:41 -0700

    🗃️ Database error handling

--- core/util/db.ts
+++ core/util/db.ts
@@ -1,4 +1,12 @@
 import mongodb from "mongoose";
 import { CONNECTION_URI } from "../.env.json";
 
-export const db = mongodb.createConnection(CONNECTION_URI);
+export const db = () => {
+  try {
+    mongodb.connect(CONNECTION_URI);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
