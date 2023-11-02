0704f915f79f3a21c93688066f71cff5dd109db7
Levi R <levir620@skiff.com>
2023-11-01 19:07:37 -0700

    🧑‍💻 Add teacher check middleware

--- core/util/middleware/admin.ts
+++ core/util/middleware/admin.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { ADMIN_KEY } from "../../.env.json";
+import { auth } from "../../auth/lucia";
 
 const checkAdmin: express.RequestHandler = async (req, res, next) => {
 	if (
@@ -11,4 +12,18 @@ const checkAdmin: express.RequestHandler = async (req, res, next) => {
 	else return next();
 };
 
-export { checkAdmin };
+const checkManager: express.RequestHandler = async (req, res, next) => {
+	if (
+		(req.query.user_hierarchy !== "teacher" &&
+			req.query.user_hierarchy !== "admin") ||
+		(req.query.user_key !== ADMIN_KEY && !req.cookies["auth_session"])
+	)
+		return res
+			.status(401)
+			.json({ error: "Missing key or hierarchy status" });
+	const session = await auth.validateSession(req.cookies["auth_session"]);
+	if (session.state === "active") return next();
+	else return res.status(403).json({ error: "Forbidden" });
+};
+
+export { checkAdmin, checkManager };
