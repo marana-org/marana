05e684b75492043af7e31351c71dd749f6f0d252
Levi R <levir620@skiff.com>
2023-11-08 17:06:38 -0800

    ✨ Add progress bar to UI

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
+	import { navigating } from '$app/stores';
+	import type { PageData } from './$types';
+	import { blur } from 'svelte/transition';
+	import NProgress from 'nprogress';
 	import '../styles/globals.css';
 	import '../styles/full-page.css';
+	import '../styles/progress.css';
 	import '@fontsource/inter';
 	import 'normalize.css';
+
+	export let data: PageData;
+
+	NProgress.configure({
+		showSpinner: false,
+		minimum: 0.1,
+		easing: 'linear',
+		speed: 500,
+		parent: 'div[data-nprogress]'
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <svelte:head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
-<div class="container full">
-	<slot />
-</div>
+{#key data.url}
+	<div
+		class="container full"
+		in:blur={{ duration: 300, delay: 300, amount: 5 }}
+		out:blur={{ duration: 300 }}
+	>
+		<slot />
+	</div>
+{/key}
 
 <style>
 	.container {
