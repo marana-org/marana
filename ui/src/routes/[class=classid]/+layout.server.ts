dd73ac80f0b0d32095369ba4cf1c73724aff4a1e
Levi R <levir620@skiff.com>
2023-11-11 21:22:23 -0800

    ✨ Add assignment view to class route

--- ui/src/routes/[class=classid]/+layout.server.ts
+++ ui/src/routes/[class=classid]/+layout.server.ts
@@ -1,12 +1,22 @@
+import { auth } from '$lib/server/lucia';
 import { dev } from '$app/environment';
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ fetch }) => {
+export const load = (async ({ fetch, params, cookies }) => {
+	const sessionId = cookies.get('auth_session');
+
+	if (!sessionId) throw new Error('No session id found');
+
+	const session = await auth.getSession(sessionId);
+	
+	if (session.state !== 'active') throw new Error('Session is inactive or invalid');
+
 	const classData = await fetch(
-		`http://${dev ? 'localhost:3000' : '// insert prod url here'}/class`
+		`http://${dev ? 'localhost:3000' : '// insert prod url here'}/class?class_id=${params.class}`
 	);
 
 	return {
-		classes: await classData.json()
+		user: session.user,
+		class: (await classData.json()).classes[0]
 	};
 }) satisfies LayoutServerLoad;
